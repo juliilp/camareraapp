0e60cde9fbd5c99460f68ac4b71024871c67d0ed
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-11-27 16:36:19 -0300

    add crud products

--- server/src/controllers/productos.controller.js
+++ server/src/controllers/productos.controller.js
@@ -1,9 +1,46 @@
+const Products = require("../models/products.model");
 
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteProduct = await Products.findByIdAndDelete(id);
+    res.status(200).json(deleteProduct);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+const editProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nombre } = req.body;
+    const editProduct = await Products.findByIdAndUpdate(id, { nombre });
+    res.status(200).json(editProduct)
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+const createProduct = async (req, res) => {
+  try {
+    const { nombre } = req.body;
+    const findProduct = await Products.findOne({ nombre });
+    if (findProduct) {
+      return res
+        .status(401)
+        .json({ error: "Ya existe un producto con ese nombre" });
+    }
+    const createProduct = await Products.create({ nombre });
+    res.status(200).json(createProduct);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+const allProducts = async (req, res) => {
+  try {
+    const allProducts = await Products.find({});
+    res.status(200).json(allProducts);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-const deleteProduct =  async (req,res) => {} 
-const editProduct =  async (req,res) => {} 
-const createProduct =  async (req,res) => {} 
-const allProducts =  async (req,res) => {} 
-
-
-module.exports = {deleteProduct, editProduct, createProduct, allProducts}
+module.exports = { deleteProduct, editProduct, createProduct, allProducts };
