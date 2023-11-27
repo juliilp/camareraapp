2b0d88479efe47f170e5f74e053aa729679fd20e
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-11-27 16:36:06 -0300

    Add auth y isAdmin

--- server/src/routes/productos.routes.js
+++ server/src/routes/productos.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const { createProduct, editProduct, deleteProduct, allProducts } = require("../controllers/productos.controller");
+const authRequired = require("../middleware/authRequired");
+const isAdmin = require("../middleware/isAdmin");
 
 const productosRouter = Router()
 
 productosRouter.get("/allProducts", allProducts)
 productosRouter.post("/createProduct", createProduct)
-productosRouter.put("/editProduct/:id", editProduct)
-productosRouter.delete("/deleteProduct/:id", deleteProduct)
+productosRouter.put("/editProduct/:id", authRequired,isAdmin ,editProduct)
+productosRouter.delete("/deleteProduct/:id",authRequired,isAdmin ,deleteProduct)
 module.exports = productosRouter
