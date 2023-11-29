507962926cad01a661effe043103c72ef9268d9b
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-11-29 20:05:09 -0300

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,12 @@ const cookieParser = require("cookie-parser")
 const express = require("express")
 const morgan = require("morgan")
 const routes = require("./routes")
+const cors = require("cors")
 const app = express()
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true, 
+  }));
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(cookieParser())
