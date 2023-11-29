1725b72baa832eb58617bc981d7baaf8219fca42
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-11-29 20:33:22 -0300

    fix default controller: Evitar multiples consultas

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ const { createDefaultUsers } = require("./src/controllers/default.controller");
 const connectDB = require("./src/libs/connectDB");
 
 const startServer = async () => {
-  try {
+    let rutaAccedida = false
+    try {
     await connectDB();
     console.log("Conexión a la base de datos exitosa");
     app.listen(3001, () => {
       console.log("Funcionando en la ruta 3001");
-      createDefaultUsers(); 
+      if(!rutaAccedida) {
+        rutaAccedida = true
+        createDefaultUsers(); 
+      }
     });
   } catch (error) {
     console.error("Error al conectar a la base de datos:", error);
--- server/src/controllers/default.controller.js
+++ server/src/controllers/default.controller.js
@@ -1,37 +1,33 @@
 const User = require("../models/user.model");
 
-let rutaAccedida = false;
-
 const createDefaultUsers = async () => {
-  if (!rutaAccedida) {
-    rutaAccedida = true;
-    const usersToCreate = [
-      {
-        nombre: "Julian",
-        email: "julianlopez43013@gmail.com",
-        password: "123456",
-        isAdmin: true,
-        isChecked: true,
-      },
-      {
-        nombre: "Account banned",
-        email: "banned@gmail.com",
-        password: "123456",
-        bannedAccount: true,
-      },
-      {
-        nombre: "Account not checked",
-        email: "notchecked@gmail.com",
-        password: "123456",
-        isChecked: false,
-      },
-    ];
 
-    try {
-      const createdUsers = await User.insertMany(usersToCreate);
-    } catch (error) {
-      console.error("Error al crear usuarios:", error.message);
-    }
+  const usersToCreate = [
+    {
+      nombre: "Julian",
+      email: "julianlopez43013@gmail.com",
+      password: "123456",
+      isAdmin: true,
+      isChecked: true,
+    },
+    {
+      nombre: "Account banned",
+      email: "banned@gmail.com",
+      password: "123456",
+      bannedAccount: true,
+    },
+    {
+      nombre: "Account not checked",
+      email: "notchecked@gmail.com",
+      password: "123456",
+      isChecked: false,
+    },
+  ];
+
+  try {
+    const createdUsers = await User.insertMany(usersToCreate);
+  } catch (error) {
+    console.error("Error al crear usuarios:", error.message);
   }
 };
 
