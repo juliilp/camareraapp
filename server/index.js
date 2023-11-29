7eb9be900927908b70266ab06e9586064bcaebbd
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-11-29 20:21:55 -0300

    controlles default

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const app = require("./src/app");
+const { createDefaultUsers } = require("./src/controllers/default.controller");
 const connectDB = require("./src/libs/connectDB");
 
 app.listen(3001, async ()=> {
     console.log("Funcionando en la ruta 3001")
     await connectDB()
 })
+
+app.on("listening", createDefaultUsers)
