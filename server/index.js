e1d2038d29d8ea71cbeba96bb29190bc72f8884c
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-11-29 20:27:32 -0300

    fix default controllers

--- server/index.js
+++ server/index.js
@@ -2,9 +2,17 @@ const app = require("./src/app");
 const { createDefaultUsers } = require("./src/controllers/default.controller");
 const connectDB = require("./src/libs/connectDB");
 
-app.listen(3001, async ()=> {
-    console.log("Funcionando en la ruta 3001")
-    await connectDB()
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    console.log("Conexión a la base de datos exitosa");
+    app.listen(3001, () => {
+      console.log("Funcionando en la ruta 3001");
+      createDefaultUsers(); 
+    });
+  } catch (error) {
+    console.error("Error al conectar a la base de datos:", error);
+  }
+};
 
-app.on("listening", createDefaultUsers)
+startServer();
