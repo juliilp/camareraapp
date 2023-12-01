82dece79a50e66dadafe047c93f1d8c6b24a9417
Julian <julianlopez4301@gmail.com>
2023-12-01 13:10:42 -0300

    saque authrequired

--- server/src/routes/mesa.routes.js
+++ server/src/routes/mesa.routes.js
@@ -1,13 +1,21 @@
 const { Router } = require("express");
-const {createMesa, editMesa, deleteMesa, deleteMesaForAdmin, allMesas, addProductMesa, MesaPorId} = require("../controllers/mesa.controller");
+const {
+  createMesa,
+  editMesa,
+  deleteMesa,
+  deleteMesaForAdmin,
+  allMesas,
+  addProductMesa,
+  MesaPorId,
+} = require("../controllers/mesa.controller");
 const authRequired = require("../middleware/authRequired");
 const isAdmin = require("../middleware/isAdmin");
-const mesaRoutes = Router()
+const mesaRoutes = Router();
 
-mesaRoutes.get("/allMesas", allMesas)
-mesaRoutes.post("/create", createMesa)
-mesaRoutes.put("/editMesa/:id", authRequired,editMesa)
-mesaRoutes.delete("/deleteMesa/:id",authRequired, isAdmin,deleteMesaForAdmin)
-mesaRoutes.put("/agregarPedido/:id", addProductMesa )
-mesaRoutes.get("/:id", MesaPorId)
-module.exports = mesaRoutes
+mesaRoutes.get("/allMesas", allMesas);
+mesaRoutes.post("/create", createMesa);
+mesaRoutes.put("/editMesa/:id", editMesa);
+mesaRoutes.delete("/deleteMesa/:id", authRequired, isAdmin, deleteMesaForAdmin);
+mesaRoutes.put("/agregarPedido/:id", addProductMesa);
+mesaRoutes.get("/:id", MesaPorId);
+module.exports = mesaRoutes;
