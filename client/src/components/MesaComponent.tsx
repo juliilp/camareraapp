303f07ba11520e2632790967e7519e4c92b9294f
Julian <julianlopez4301@gmail.com>
2023-12-01 18:42:16 -0300

    tailwind puesto

--- client/src/components/MesaComponent.tsx
+++ client/src/components/MesaComponent.tsx
@@ -3,7 +3,7 @@ import MesaInterface from "../interfaces/Mesa";
 
 export default function MesaComponent({ _id, numeroMesa }: MesaInterface) {
   return (
-    <section className="w-full max-w-[300px] h-56 border justify-center items-center flex ">
+    <section className="w-full max-w-[300px] h-56 border border-black justify-center items-center flex ">
       <Link to={`mesa/${_id}`}>
         <span className="">{numeroMesa}</span>
       </Link>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.ts'
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  
-  <Provider store={store} >
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
+import "tailwindcss/tailwind.css";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <App />
-    
-    </Provider>
-)
+  </Provider>
+);
