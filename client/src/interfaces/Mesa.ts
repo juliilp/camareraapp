bc9124aa9d0cf4763ad56c0617cabac329d273ee
Julian <julianlopez4301@gmail.com>
2023-12-01 18:28:00 -0300

    agregue una nuevo parametro

--- client/src/interfaces/Mesa.ts
+++ client/src/interfaces/Mesa.ts
@@ -5,6 +5,7 @@ interface MesaInterface {
   pedidoParaEntregar?: boolean;
   numeroMesa?: number;
   ___v?: number;
+  handlerMesaActualizada?: (id: string | undefined) => void;
 }
 
 export default MesaInterface;
