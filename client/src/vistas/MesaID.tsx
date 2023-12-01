990cbf786d44c1dba6ad3f8aef44b9a7c41d31b4
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-11-30 22:49:01 -0300

    Mesa por ID

--- client/src/vistas/MesaID.tsx
+++ client/src/vistas/MesaID.tsx
@@ -1,5 +1,20 @@
-
+import axios from "axios"
+import { useParams } from "react-router-dom"
+import {useEffect, useState} from 'react'
 export default function MesaID() {
+  const {id} = useParams()
+  const [mesaId, setMesaId] = useState()
+
+  useEffect(() => {
+    
+    async function peticionMesa() {
+      const res = await axios.get("/mesa/" + id)
+      console.log(res)
+      setMesaId(res.data)
+    }
+    peticionMesa()
+  }, [])
+  console.log(mesaId)
   return (
     <div>MesaID</div>
   )
