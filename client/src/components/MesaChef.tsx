299a25eb540bf33c7b8558353f41dfbac534baa7
Julian <julianlopez4301@gmail.com>
2023-12-01 18:28:12 -0300

    /chef funcionando

---
+++ client/src/components/MesaChef.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import MesaInterface from "../interfaces/Mesa";
+
+interface MesaChefProps extends MesaInterface {
+  handlerMesaActualizada?: (id: string | undefined) => void;
+}
+
+export default function MesaChef({
+  pedido,
+  _id,
+  handlerMesaActualizada,
+  numeroMesa,
+}: MesaChefProps) {
+  const handlerMesaTerminada = async () => {
+    const responseConfirm = confirm(
+      `Estas seguro de terminar el pedido de la mesa ${numeroMesa}?`
+    );
+    if (responseConfirm) {
+      const res = await axios.put(`mesa/editMesa/${_id}`, {
+        pedidoParaEntregar: true,
+        pedidoListo: false,
+        pedido: [],
+      });
+      if (res.status === 200 && typeof handlerMesaActualizada === "function") {
+        handlerMesaActualizada(_id);
+      }
+    }
+  };
+
+  return (
+    <section className="border  h-[250px] w-[250px]">
+      Mesa número: {numeroMesa}
+      <ul>
+        {pedido?.map((p, index) => (
+          <li key={index}>{p}</li>
+        ))}
+      </ul>
+      <button onClick={handlerMesaTerminada}>Mesa terminada</button>
+    </section>
+  );
+}
--- client/src/vistas/Chef.tsx
+++ client/src/vistas/Chef.tsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react";
+import useMesa from "../hooks/useMesa";
+import MesaInterface from "../interfaces/Mesa";
+import MesaChef from "../components/MesaChef";
 
 export default function Chef() {
+  const [mesasListas, setMesasListas] = useState<MesaInterface[]>();
+  const { data, isError, isLoading, isSuccess } = useMesa();
+
+  useEffect(() => {
+    if (isSuccess) {
+      const mesasFiltradas = data?.filter((m) => m.pedidoListo === true);
+      setMesasListas(mesasFiltradas);
+    }
+  }, [isLoading, isSuccess]);
+
+  const handleMesaActualizada = (mesaId: string | undefined) => {
+    setMesasListas(
+      (prevMesas) => prevMesas && prevMesas.filter((m) => m._id !== mesaId)
+    );
+  };
   return (
-    <div>Chef</div>
-  )
+    <section>
+      {isError && <p>Error</p>}
+      {isLoading && <p>Cargando</p>}
+
+      {isSuccess &&
+        mesasListas?.map((m: MesaInterface) => (
+          <MesaChef
+            key={m._id}
+            pedido={m.pedido}
+            _id={m._id}
+            numeroMesa={m.numeroMesa}
+            handlerMesaActualizada={() => handleMesaActualizada(m._id)}
+          />
+        ))}
+    </section>
+  );
 }
