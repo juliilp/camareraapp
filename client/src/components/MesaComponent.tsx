7c2d49235310a22252681ddb76993c1d9a0a8233
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-02 22:40:14 -0300

    Avisa si la mesa Tiene pedido

--- client/src/components/MesaComponent.tsx
+++ client/src/components/MesaComponent.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import MesaInterface from "../interfaces/Mesa";
 
-export default function MesaComponent({ _id, numeroMesa }: MesaInterface) {
+export default function MesaComponent({ _id, numeroMesa, pedido }: MesaInterface) {
   return (
-    <section className="w-full max-w-[300px] h-56 border border-black justify-center items-center flex ">
+    <section className={"w-full max-w-[300px] h-56 border border-black justify-center items-center flex"}>
       <Link to={`mesa/${_id}`}>
         <span className="">{numeroMesa}</span>
       </Link>
+      {
+        pedido?.length! > 0 && <span>Tiene pedido</span>
+      }
     </section>
   );
 }
