ecebf8560a311c322821ae95b87200e737eff01c
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-02 22:41:02 -0300

    Contador que avisa si hay que buscar pedido

--- client/src/vistas/Home.tsx
+++ client/src/vistas/Home.tsx
@@ -1,21 +1,32 @@
+import { useEffect, useState } from "react";
 import MesaComponent from "../components/MesaComponent";
 import useMesa from "../hooks/useMesa";
-import useUsers from "../hooks/useUsers";
+// import useUsers from "../hooks/useUsers";
 
 export default function Home() {
   const { data, isError, isLoading, isSuccess } = useMesa();
-  const {user, isAuthenticate} = useUsers()
-  console.log(user, isAuthenticate)
+  // const { user, isAuthenticate } = useUsers();
+  const [pedidoListo, setPedidoListo] = useState<number>(0);
+
+  useEffect(() => {
+    const pedidos = data && data?.filter((m) => m.pedidoParaEntregar === true);
+    setPedidoListo(pedidos ? pedidos.length : 0);
+  }, [data]);
+  
   return (
     <main>
+      <span className="font-semibold text-xl fixed top-0 right-6" >Pedidos Terminados: {pedidoListo}</span>
       {isError && <p>Error</p>}
       {isLoading && <p>is Loading</p>}
       {isSuccess &&
-        data?.map((m) => {
-          return (
-            <MesaComponent key={m._id} numeroMesa={m.numeroMesa} _id={m._id} />
-          );
-        })}
+        data?.map((m) => (
+          <MesaComponent
+            key={m._id}
+            numeroMesa={m.numeroMesa}
+            _id={m._id}
+            pedido={m.pedido}
+          />
+        ))}
     </main>
   );
 }
