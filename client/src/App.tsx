1764df9d5b2e8a1f14fc84e852bf040c0465704f
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-03 15:53:02 -0300

    usando browserRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DashBoard from "./vistas/DashBoard";
 import Home from "./vistas/Home";
 import Login from "./vistas/Login";
@@ -11,71 +11,63 @@ import axios from "axios";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
 import { useDispatch } from "react-redux";
-import { addUser, AuthenticateFalse, AuthenticateTrue } from "./redux/slices/userSlice";
+import {
+  addUser,
+  AuthenticateFalse,
+  AuthenticateTrue,
+} from "./redux/slices/userSlice";
 import RutaProtegidaAdmin from "./utils/RutaProtegidaAdmin";
+import RutaUserAuthenticate from "./utils/RutaUserAuthenticate";
 axios.defaults.baseURL = "http://localhost:3001/";
 export default function App() {
-  const routes = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/dashboard",
-      element: <RutaProtegidaAdmin />,
-      children: [
-        {
-          path:"",
-          element:  <DashBoard/>
-        }
-      ]
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/registro",
-      element: <Registro />,
-    },
-    {
-      path: "/chef",
-      element: <Chef />,
-    },
-    {
-      path: "/pedidoListo",
-      element: <PedidoListo />,
-    },
-    {
-      path: "/mesa/:id",
-      element: <MesaID />,
-    },
-  ]);
   const queryClient = new QueryClient();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
-   async  function sessionUser() {
-      const token = Cookies.get("token")
-    
-      if(token) {
+    async function sessionUser() {
+      const token = Cookies.get("token");
+
+      if (token) {
         try {
-          const res = await axios.get("/user/verifyToken", {withCredentials: true})
-          dispatch(addUser(res.data))
-          dispatch(AuthenticateTrue())
+          const res = await axios.get("/user/verifyToken", {
+            withCredentials: true,
+          });
+          dispatch(addUser(res.data));
+          dispatch(AuthenticateTrue());
         } catch (error) {
-          dispatch(AuthenticateFalse())
-          console.log(error)
+          dispatch(AuthenticateFalse());
+          console.log(error);
         }
       }
-  
     }
-    sessionUser()
+    sessionUser();
   }, []);
   return (
-    <>
+    
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={routes} />
+      <BrowserRouter>
+      <Routes>
+      <Route  path="/" element={ <Home />} />
+      <Route path="/dashboard" element={
+         <RutaProtegidaAdmin>
+         <DashBoard />
+       </RutaProtegidaAdmin>
+      }  />
+      <Route path="/login" element={
+        <RutaUserAuthenticate>
+          <Login />
+        </RutaUserAuthenticate>} />
+      
+      <Route path="/registro" element={
+         <RutaUserAuthenticate>
+         <Registro />
+       </RutaUserAuthenticate>
+      } />
+      <Route path="/chef" element={<Chef />} />
+      <Route path="/pedidolisto" element={<PedidoListo />} />
+      <Route path="/mesa/:id" element={<MesaID />} />
+      </Routes>
+      </BrowserRouter>
       </QueryClientProvider>
-    </>
+    
   );
 }
