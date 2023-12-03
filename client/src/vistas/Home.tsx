88ddc37fe947dc042463b43a4ea89c86790e5256
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-03 13:20:15 -0300

    Codigo mas limpio

--- client/src/vistas/Home.tsx
+++ client/src/vistas/Home.tsx
@@ -1,24 +1,10 @@
-import { useEffect, useState } from "react";
 import MesaComponent from "../components/MesaComponent";
 import useMesa from "../hooks/useMesa";
-import { Link } from "react-router-dom";
-// import useUsers from "../hooks/useUsers";
-
 export default function Home() {
   const { data, isError, isLoading, isSuccess } = useMesa();
-  // const { user, isAuthenticate } = useUsers();
-  const [pedidoListo, setPedidoListo] = useState<number>(0);
 
-  useEffect(() => {
-    const pedidos = data && data?.filter((m) => m.pedidoParaEntregar === true);
-    setPedidoListo(pedidos ? pedidos.length : 0);
-  }, [data]);
-  
   return (
-    <main>
-      <Link to="/pedidoListo" >
-      <span className="font-semibold text-xl fixed top-0 right-6" >Pedidos Terminados: {pedidoListo}</span>
-      </Link>
+    <main> 
       {isError && <p>Error</p>}
       {isLoading && <p>is Loading</p>}
       {isSuccess &&
